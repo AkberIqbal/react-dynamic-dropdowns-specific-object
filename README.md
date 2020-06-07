e15c0d42543d542ef3bafe55b06eb67828757f38
AkberIqbal <akber.iqbal@gmail.com>
2020-06-07 11:21:20 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-rr3w3r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rr3w3r)
---
+++ dropdown.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+
+const countries = {
+  France: ["Paris", "Marseille", "Lille", "Lyon"],
+  Usa: ["New York", "San Francisco", "Austin", "Dallas"]
+};
+
+const Dropdown = () => {
+  const [countryData, setCountryData] = useState(["Usa"]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  const checkInsertInArray = newCountry => {
+    let findStatus = countryData.find(x => {
+      return x === newCountry;
+    });
+    if (!findStatus) {
+      setCountryData([...countryData, newCountry]);
+    }
+  };
+
+  const countryChange = event => {
+    if (event.target.value) {
+      setSelectedCountry(event.target.value);
+    }
+  };
+
+  useEffect(() => {
+    Object.keys(countries).forEach(country => {
+      checkInsertInArray(country);
+    });
+  });
+
+  return (
+    <>
+      <span>Country:</span>
+      <select onChange={countryChange}>
+        <option value="" />
+        {countryData.map(allCountries => {
+          return <option value={allCountries}>{allCountries}</option>;
+        })}
+      </select>
+      <br />
+      {selectedCountry ? (
+        <>
+          <span>City:</span>{" "}
+          <select>
+            <option value="" />
+            {countries[selectedCountry].map(allCountries => {
+              return <option value={allCountries}>{allCountries}</option>;
+            })}
+          </select>
+        </>
+      ) : (
+        <span>City: Please select a country first</span>
+      )}
+    </>
+  );
+};
+
+export default Dropdown;
